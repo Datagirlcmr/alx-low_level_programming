00248876260151320b7ffa8df71f569659ed23fe
Datagirl <datagirlcmr@gmail.com>
2023-04-21 23:03:52 +0100

    additional tasks

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Succes)
+ */
+int main(void)
+{
+	int x;
+	int y;
+
+	for (x = 48; x <= 57; x++)
+	{
+		for (y = 49; y <= 57; y++)
+		{
+			if (x != y && y > x)
+			{
+				putchar(x);
+				putchar(y);
+				putchar(',');
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
