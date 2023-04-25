e49bd7bf62c5eacd759d0d7bfd784d3e8d7b39df
Datagirl <datagirlcmr@gmail.com>
2023-04-25 06:57:54 +0100

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+/**
+  * add - Entry point.
+  * @i: arguments that is added
+  * @k: argument that is added
+  * Description: a function that adds two integers.
+  * Return: Always 0 (Success)
+  */
+int add(int i, int k)
+{
+return (i + k);
+}
