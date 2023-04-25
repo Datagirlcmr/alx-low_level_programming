25214fef9f965b26f19efcb02f05f8f5d33f0f99
Datagirl <datagirlcmr@gmail.com>
2023-04-25 07:03:43 +0100

    11-print_to_98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,22 @@
+/**
+  * print_to_98 - prints all natural numbers from n to 98
+  * @n: argument to be checked
+ */
+
+void print_to_98(int n)
+{
+
+while (n < 98)
+{
+printf("%i, ", n);
+n++;
+}
+
+while (n > 98)
+{
+printf("%i, ", n);
+n--;
+}
+printf("98");
+putchar('\n');
+}
