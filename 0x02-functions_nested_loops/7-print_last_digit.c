474904a68ac0e8bee455397b723ec58b8f3f6582
Datagirl <datagirlcmr@gmail.com>
2023-04-25 06:40:21 +0100

    7-print-last-digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+/**
+  * print_last_digit - Entry point.
+  * @a: argument that is checked
+  * Description: a function that prints the last digit.
+  * Return: Always 0 (Success)
+  */
+int print_last_digit(int a)
+{
+if (a >= 0 && a <= 9)
+return (a);
+
+else
+{
+return (_abs(a % 10));
+}
+}
