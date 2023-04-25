d242fc5c132484a6affcc6913754b7795c6614da
Datagirl <datagirlcmr@gmail.com>
2023-04-25 07:11:48 +0100

    100-times_table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,48 @@
+/**
+ * print_times_table - Entry point.
+ * @n: argument that is checked
+ * Description: a function that prints the n times table, starting with 0.
+ * Return: Always 0 (Success)
+ */
+void print_times_table(int n)
+{
+int a = 0, rep, b;
+
+if (n < 0 || n > 15)
+return;
+
+while (a <= n)
+{
+for (b = 0; b <= n; b++)
+{
+rep = a * b;
+if  (b == 0)
+_putchar('0' + rep);
+else if (rep < 10)
+{
+_putchar(' ');
+_putchar(' ');
+_putchar('0' + rep);
+}
+else if (rep < 100)
+{
+_putchar(' ');
+_putchar('0' + rep / 10);
+_putchar('0' + rep % 10);
+}
+else
+{
+_putchar('0' + rep / 100);
+_putchar('0' + (rep - 100) / 10);
+_putchar('0' + rep % 10);
+}
+if (b < n)
+{
+_putchar(',');
+_putchar(' ');
+}
+}
+_putchar('\n');
+a++;
+}
+}
