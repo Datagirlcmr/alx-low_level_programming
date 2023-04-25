d11b423b84414c3e6de26824bbe8ed8bda0f66b8
Datagirl <datagirlcmr@gmail.com>
2023-04-25 06:45:34 +0100

    8-24_hours

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,21 @@
+/**
+  * jack_bauer - print every minute of the 24 hour clock
+  */
+
+void jack_bauer(void)
+{
+int a, b;
+
+for (a = 0; a <= 23; a++)
+{
+for (b = 0; b <= 59; b++)
+{
+_putchar (a / 10 + '0');
+_putchar (a % 10 + '0');
+_putchar (':');
+_putchar (b / 10 + '0');
+_putchar (b % 10 + '0');
+_putchar ('\n');
+}
+}
+}
