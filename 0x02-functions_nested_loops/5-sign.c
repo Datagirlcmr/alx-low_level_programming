8819750d0dbae93a289d8ce44b71353b0d623459
Datagirl <datagirlcmr@gmail.com>
2023-04-25 06:27:03 +0100

    6-abs.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+/**
+  * print_sign - Entry point.
+  * @n: argument that is checked
+  * Description: a function that prints the sign of a number.
+  * Return: Always 0 (Success)
+  */
+int print_sign(int n)
+{
+int test;
+
+if (n > 0)
+{
+test = 1;
+_putchar('+');
+}
+else if (n == 0)
+{
+test = 0;
+_putchar('0');
+}
+else
+{
+test = -1;
+_putchar('-');
+}
+return (test);
+}
