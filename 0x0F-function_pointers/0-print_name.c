002896572f80232f6b1db45ee44e222bf4e21622
Datagirl <datagirlcmr@gmail.com>
2023-05-24 11:18:50 +0100

    0. What's my name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "function_pointers.h"
 
 /**
@@ -9,7 +10,11 @@
 void print_name(char *name, void (*f)(char *))
 {
 	if (name == NULL || f == NULL)
-	
+	{
+		return;
+	}
+	else
+	{
 		f(name);
 	}
 }
