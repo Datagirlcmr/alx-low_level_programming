930f1b7f833e7b934833d8f94c88cb51e943ab87
Datagirl <datagirlcmr@gmail.com>
2023-08-16 04:13:47 +0100

    update task 1

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,24 +1,26 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_create - creates a hash table
- * @size: size of the array
- * Return: a pointer to the newly created hash table
-**/
-
+ * hash_table_create - Creates a hash table
+ * @size: The size of the array
+ * Return: A pointer to the newly created hash table, NULL if something goes
+ * wrong
+ */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *ht;
+	hash_table_t *ht = NULL;
 
-	if (!size)
+	if (!size || size == 0)
 		return (NULL);
 
-	ht = calloc(1, sizeof(hash_table_t));
-	if (!ht)
+	ht = malloc(sizeof(hash_table_t));
+	if (ht == NULL)
 		return (NULL);
+
 	ht->size = size;
+
 	ht->array = calloc(size, sizeof(hash_node_t *));
-	if (!ht->array)
+	if (ht->array == NULL)
 	{
 		free(ht);
 		return (NULL);
